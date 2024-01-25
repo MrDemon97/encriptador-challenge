b6fee91be715980c1ae12a1e28aa57ec459c4276
JairCoral <jaircoral88@gmail.com>
2024-01-25 12:52:47 -0500

    Validacion de texto ingresado

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 let textIngresado;
-textIngresado = "hola mi nombre es jair"
+/* creamos una constante en la cual almacenamos una expresión regular 
+NOTA : / Delimita el inicio y final de la expresión
+       ^ Inica el inicio de la línea
+       $ Indica el final de línea
+       + Indica que el patrón debe aparecer por lo
+          menos una vez
+       []Define caracteres que se van a incluir en el conjunto
+       a-z carácteres minúsculos del alfabeto
+       \s carácter que le dice al pc que es un espacion en blanco
+        */    
+const textoValido = /^[a-z\s]+$/
 
+function encriptar(){
+    validarTexto()
+
+}
+
+function desencriptar(){
+
+}
+
+function validarTexto(){
+    //selecciona el elemento textArea, de donde sacaremos el valor;
+    textIngresado = document.querySelector(".texto-a-analizar").value;
+    //verificamos si lo que ingresó el usuario
+    if(textIngresado === ""){
+        alert("No ha ingresado ningun tipo de texto");
+    
+        //suaremos la funcion test para comprobar si todo está en minúsculas y sin acentos
+    }else if(!textoValido.test(textIngresado)){
+              
+        alert("el texto ingesado solo debe estar compuesto por minúsculas y sin acentos");
+    }else{
+        alert(textIngresado);
+    }
+}
 
